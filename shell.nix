1b66015959b9bf014f6da850a8a54cb15a68c7f4
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-06-28 09:22:51 -0700

    Setup basics

---
+++ shell.nix
@@ -0,0 +1,51 @@
+let
+
+  sources = import ./nix/sources.nix;
+  pkgs    = import sources.nixpkgs {};
+
+  commands = import ./nix/commands.nix;
+  tasks = commands {pkgs = pkgs;};
+
+  deps = {
+    tools = [
+      pkgs.curl
+      pkgs.devd
+      pkgs.just
+      pkgs.watchexec
+    ];
+
+    elm = [
+      pkgs.elmPackages.elm
+      pkgs.elmPackages.elm-format
+      pkgs.elmPackages.elm-live
+    ];
+
+    node = [
+      pkgs.nodejs-14_x
+      pkgs.nodePackages.pnpm
+    ];
+
+    fun = [
+      pkgs.figlet
+      pkgs.lolcat
+    ];
+  };
+
+in
+
+  pkgs.mkShell {
+    nativeBuildInputs = builtins.concatLists [
+      deps.tools
+      deps.elm
+      deps.node
+      deps.fun
+      tasks
+    ];
+
+    shellHook = ''
+      export LANG=C.UTF8
+
+      echo "🌈✨ Welcome to the glorious... "
+      ${pkgs.figlet}/bin/figlet "Fission Drive Env" | lolcat -a -s 50
+    '';
+  }
