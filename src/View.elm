6efa04b8c8960f97457d0f22fee0101cbdff32ff
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-30 13:10:03 +0200

    Implement loading screen

--- src/View.elm
+++ src/View.elm
@@ -4,16 +4,72 @@ import Css
 import Html.Styled as Html exposing (..)
 import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events as Events
+import Tailwind.Breakpoints exposing (..)
 import Tailwind.Utilities exposing (..)
 
 
 test : msg -> Html msg
 test noOp =
-    signinScreen { onClickSignIn = noOp }
+    -- signinScreen { onClickSignIn = noOp }
+    loadingScreen { message = "Spinning violently around the y-axis..." }
+
+
+loadingScreen : { message : String } -> Html msg
+loadingScreen element =
+    appShellCentered
+        [ moonGarden
+        , p
+            [ css
+                [ font_body
+                , text_bluegray_700
+                , flex
+                , flex_col
+                , items_center
+                , space_y_2
+                ]
+            ]
+            [ span
+                [ css
+                    [ text_2xl
+                    , animate_spin
+                    ]
+                ]
+                [ text "🌕" ]
+            , span [] [ text element.message ]
+            ]
+        ]
 
 
 signinScreen : { onClickSignIn : msg } -> Html msg
 signinScreen element =
+    appShellCentered
+        [ moonGarden
+        , p
+            [ css
+                [ text_bluegray_800
+                , text_center
+                , font_body
+                ]
+            ]
+            [ text "is a digital garden app built on the Fission Platform."
+            , br [] []
+            , br [] []
+            , text "Start your own digital garden by logging in to your fission account or creating a new one:"
+            ]
+        , button
+            [ Events.onClick element.onClickSignIn
+            , css
+                [ px_12
+                , py_3
+                , leafButtonStyle
+                ]
+            ]
+            [ text "Sign in With Fission" ]
+        ]
+
+
+appShellCentered : List (Html msg) -> Html msg
+appShellCentered content =
     div
         [ css
             [ bg_beige_100
@@ -34,38 +90,22 @@ signinScreen element =
                 , space_y_8
                 ]
             ]
-            [ h1
-                [ css
-                    [ text_bluegray_800
-                    , text_center
-                    , text_5xl
-                    , font_title
-                    ]
-                ]
-                [ text "Moon Garden" ]
-            , p
-                [ css
-                    [ text_bluegray_800
-                    , text_center
-                    , font_body
-                    ]
-                ]
-                [ text "is a digital garden app built on the Fission Platform."
-                , br [] []
-                , br [] []
-                , text "Start your own digital garden by logging in to your fission account or creating a new one:"
-                ]
-            , button
-                [ Events.onClick element.onClickSignIn
-                , css
-                    [ px_12
-                    , py_3
-                    , leafButtonStyle
-                    ]
-                ]
-                [ text "Sign in With Fission" ]
+            content
+        ]
+
+
+moonGarden : Html msg
+moonGarden =
+    h1
+        [ css
+            [ text_bluegray_800
+            , text_center
+            , text_2xl
+            , font_title
+            , sm [ text_5xl ]
             ]
         ]
+        [ text "Moon Garden" ]
 
 
 leafButtonStyle : Css.Style
