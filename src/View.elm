36060267af70cc02581bc61c5655291be631ed52
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-30 13:57:57 +0200

    Do search results

--- src/View.elm
+++ src/View.elm
@@ -1,8 +1,9 @@
 module View exposing (..)
 
 import Css
+import FeatherIcons
 import Html.Styled as Html exposing (..)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes exposing (css, href, id, placeholder, type_)
 import Html.Styled.Events as Events
 import Tailwind.Breakpoints exposing (..)
 import Tailwind.Utilities exposing (..)
@@ -11,7 +12,8 @@ import Tailwind.Utilities exposing (..)
 test : msg -> Html msg
 test noOp =
     -- signinScreen { onClickSignIn = noOp }
-    loadingScreen { message = "Spinning violently around the y-axis..." }
+    -- loadingScreen { message = "Spinning violently around the y-axis..." }
+    appShellSidebar
 
 
 loadingScreen : { message : String } -> Html msg
@@ -94,6 +96,83 @@ appShellCentered content =
         ]
 
 
+appShellSidebar : Html msg
+appShellSidebar =
+    div
+        [ css
+            [ bg_beige_100
+            , flex
+            , flex_row
+            , flex_grow
+            , flex_shrink_0
+            , overflow_x_auto
+            , Css.property "scroll-snap-type" "x mandatory"
+            ]
+        ]
+        [ div
+            [ css
+                [ w_64
+                , py_6
+                , flex_shrink_0
+                , Css.property "scroll-snap-align" "start"
+                , overflow_y_auto
+                , flex
+                , flex_col
+                ]
+            ]
+            [ div
+                [ css
+                    [ border_beige_300
+                    , border_r_4
+                    , px_5
+                    , flex
+                    , flex_col
+                    , flex_grow
+                    ]
+                ]
+                (List.concat
+                    [ [ button
+                            [ css
+                                [ leafButtonStyle
+                                , py_3
+                                ]
+                            ]
+                            [ text "Create New Note" ]
+                      , searchInput [ mt_8 ]
+                      ]
+                    , List.map
+                        (\result ->
+                            searchResult
+                                { label = result
+                                , link = "#dashboard"
+                                , styles = [ mt_4 ]
+                                }
+                        )
+                        [ "Moon Garden"
+                        , "Markdown"
+                        , "WNFS"
+                        , "Wikilinks"
+                        , "Geometric Algebra for Computer Science"
+                        ]
+                    ]
+                )
+            ]
+        , div
+            [ id "dashboard"
+            , css
+                [ p_6
+                , Css.property "min-width" "100vw"
+                , flex_grow
+                , flex_shrink_0
+                , h_full
+                , Css.property "scroll-snap-align" "start"
+                , overflow_y_auto
+                ]
+            ]
+            [ moonGarden ]
+        ]
+
+
 moonGarden : Html msg
 moonGarden =
     h1
@@ -106,6 +185,84 @@ moonGarden =
             ]
         ]
         [ text "Moon Garden" ]
+
+
+searchInput : List Css.Style -> Html msg
+searchInput styles =
+    div
+        [ css
+            [ relative
+            , flex
+            , flex_col
+            , Css.batch styles
+            ]
+        ]
+        [ input
+            [ type_ "text"
+            , placeholder "Type to Search"
+            , css
+                [ px_4
+                , py_3
+                , rounded_md
+                , bg_beige_200
+                , focusable
+                , font_mono
+                , Css.pseudoElement "placeholder"
+                    [ text_beige_400
+                    ]
+                ]
+            ]
+            []
+        , FeatherIcons.search
+            |> FeatherIcons.withSize 20
+            |> FeatherIcons.toHtml []
+            |> fromUnstyled
+            |> List.singleton
+            |> span
+                [ css
+                    [ text_beige_400
+                    , my_auto
+                    ]
+                ]
+            |> List.singleton
+            |> div
+                [ css
+                    [ absolute
+                    , pointer_events_none
+                    , right_4
+                    , inset_y_0
+                    , flex
+                    , flex_row
+                    ]
+                ]
+        ]
+
+
+searchResult :
+    { label : String
+    , link : String
+    , styles : List Css.Style
+    }
+    -> Html msg
+searchResult element =
+    a
+        [ href element.link
+        , css
+            [ Css.batch element.styles
+            , rounded_md
+            , bg_beige_200
+            , px_4
+            , py_3
+            , focusable
+            , font_body
+            , text_bluegray_800
+            , transform_gpu
+            , transition_transform
+            , Css.active
+                [ scale_95 ]
+            ]
+        ]
+        [ text element.label ]
 
 
 leafButtonStyle : Css.Style
