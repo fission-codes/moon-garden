28a783cdbf20762b6d76ef4f3cc8783b493af87b
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-06-29 19:10:27 -0700

    Horribly broken state, but WIP before dinner

--- src/Main.elm
+++ src/Main.elm
@@ -6,13 +6,32 @@ import Html.Styled.Attributes exposing (css)
 import Ports
 import Tailwind.Utilities exposing (..)
 
-import Msg exposing (Msg (..))
-import Model exposing (Model (..))
-import View exposing (view)
+
+-- 🔠 ---------------------------------------------------------------------------
+
+
+type Model
+    = Unauthed Unauthenticated
+    | Authenticated
+
+
+type Unauthenticated
+    = Loading
+    | Cancelled
+    | SignIn
+
+
+type Msg
+    = NoOp -- FIXME Replace with navigation messages
+    | WebnativeSignIn
+    | WebnativeInit Bool
 
 
 type alias Flags =
     ()
+
+
+-- 〽️  --------------------------------------------------------------------------
 
 
 main : Program Flags Model Msg
@@ -27,24 +46,88 @@ main =
         }
 
 
+-- 🔁 --------------------------------------------------------------------------
+
+
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
 
-        WebnativeInit isAuthenticated ->
-            ( authFromBool isAuthenticated
-            , Cmd.none
-            )
+                -- FIXME check if already logged in
+        WebnativeInit isAuthed ->
+            ( initAuthState isAuthed, Cmd.none )
 
-authFromBool : Bool -> Model
-authFromBool isAuthenticated =
-    if isAuthenticated then
-        Authenticated
-    else
-        NotAuthenticated
+        WebnativeSignIn ->
+            ( model, Ports.redirectToLobby () )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Ports.webnativeInit WebnativeInit
+
+
+initAuthState : Bool -> Model
+initAuthState isAuthenticated =
+    if isAuthenticated then
+        Authenticated
+    else
+        Unauthenticated SignIn
+
+
+-- 🖼️  ---------------------------------------------------------------------------
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "🌛 Moon Garden 🌱"
+    , body = [ Html.toUnstyled <| body model ]
+    }
+
+body : Model -> Html Msg
+body model =
+    case model of
+        Unauthenticated _ -> unauthenticated
+        Authenticated -> authenticated
+
+authenticated : Html Msg
+authenticated =
+    mainContainer
+        [ Html.h1 [] [ Html.text "yay logged in" ]
+        ]
+
+
+unauthenticated : Html Msg
+unauthenticated =
+    mainContainer
+        [ Html.h1
+            [ css
+                [ font_title
+                , text_4xl
+                , font_thin
+                ]
+            ]
+            [ Html.text "Welcome to 🌛 Moon Garden! 🌱" ]
+        , Html.p
+            [ css
+                [ font_body
+                , mt_6
+                ]
+            ]
+            [ Html.text "A digital garden / second brain, built on Fission. Please take a seat and plant a seed." ]
+
+        , Html.button [ css [ bg_gray_50 ] ]
+            [ Html.text "Sign in with Fission" ]
+        ]
+
+mainContainer : List (Html Msg) -> Html Msg
+mainContainer =
+    Html.main_
+        [ css
+            [ p_6
+            , text_bluegray_800
+            , bg_beige_100
+            , flex_grow
+            ]
+        ]
