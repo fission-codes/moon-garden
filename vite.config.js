a6ddcb0f8ddb188ef6678f722bf9db4433f48cab
Philipp Krüger <philipp.krueger1@gmail.com>
2021-07-01 10:51:04 +0200

    Write build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import * as elmPlugin from 'vite-plugin-elm'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [elmPlugin.plugin()],
+  build: {
+    target: 'es2020',
+    rollupOptions: {
+      input: {
+        main: resolve(currentDir, 'index.html'),
+        view_test: resolve(currentDir, 'view_test.html')
+      }
+    }
+  }
 })
